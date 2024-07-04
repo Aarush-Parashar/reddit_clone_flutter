9d1e475928d4bbb9a7c6a494e0f8ae943e8ead87
Aarush-Parashar <aarushparashar2004@gmail.com>
2024-07-04 17:40:07 +0530

    added google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone_flutter/features/auth/screen/login_screen.dart';
 import 'package:reddit_clone_flutter/firebase_options.dart';
 import 'package:reddit_clone_flutter/theme/pallete.dart';
@@ -9,7 +10,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
